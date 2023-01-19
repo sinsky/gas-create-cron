d4ebe9321bdba3b8550b9bca5d9447f3dbe67631
sinsky <60662802+sinsky@users.noreply.github.com>
2023-01-19 11:34:25 +0900

    create test, convertEndDateToDateList

--- test/parseDate.test.ts
+++ test/parseDate.test.ts
@@ -1,4 +1,5 @@
 import parseDate, { convertEndDateToDateList, convertLimitToDateList } from "../src/parseDate";
+import { parseExpression } from "cron-parser";
 
 const date = new Date("2023/1/1 00:00:00");
 
@@ -39,6 +40,39 @@ test.concurrent.each([
   expect(dateList.map((date) => date.toUTCString())).toStrictEqual(idealDateList.map((date) => date.toUTCString()));
 });
 
-test.todo("convertEndDateToDateList");
+test("convertEndDateToDateList - pattern 1", () => {
+  const currentDate = new Date("2023/1/1 00:00");
+  const endDate = new Date("2023/6/30 23:59");
+  const cron = parseExpression("0 0 1 * *", { ...defaultOptions, currentDate, endDate });
+  const result = convertEndDateToDateList(cron);
+
+  expect(result.length).toBe(5);
+
+  // generate Date with Feb from Jun
+  const ideaDateList = [2, 3, 4, 5, 6].map((monthNumber) => new Date(new Date("2023/1/1 00:00").setMonth(monthNumber - 1)));
+  // [(new Date("2023/2/1 00:00"), new Date("2023/3/1 00:00"), new Date("2023/4/1 00:00"), new Date("2023/5/1 00:00"), new Date("2023/6/1 00:00"))];
+  const result_UTCStringList = result.map((date) => date.toUTCString());
+  const idealList_UTCStringList = ideaDateList.map((date) => date.toUTCString());
+  for (let i = 0; i < result.length; i++) {
+    expect(result_UTCStringList[i]).toStrictEqual(idealList_UTCStringList[i]);
+  }
+});
+
+test("convertEndDateToDateList - pattern 2", () => {
+  const currentDate = new Date("2023/1/1 00:00");
+  const cron = parseExpression("0 0 1 * *", { ...defaultOptions, currentDate });
+  const result = convertEndDateToDateList(cron);
+
+  expect(result.length).toBe(20);
+
+  // generate Date with 2022 Feb from 2024 Sep
+  const ideaDateList = [...[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((monthNumber) => new Date(new Date("2023/1/1 00:00").setMonth(monthNumber - 1))), ...[1, 2, 3, 4, 5, 6, 7, 8, 9].map((monthNumber) => new Date(new Date("2024/1/1 00:00").setMonth(monthNumber - 1)))];
+
+  const result_UTCStringList = result.map((date) => date.toUTCString());
+  const idealList_UTCStringList = ideaDateList.map((date) => date.toUTCString());
+  for (let i = 0; i < result.length; i++) {
+    expect(result_UTCStringList[i]).toStrictEqual(idealList_UTCStringList[i]);
+  }
+});
 
 test.todo("convertLimitToDateList");
