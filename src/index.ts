f7d25aeb6cd2dc0ce342921cff1d8382436f6fd4
sinsky <60662802+sinsky@users.noreply.github.com>
2023-01-19 10:23:06 +0900

    set index.ts, created parseDate function

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ declare const global: {
   [x: string]: any;
 };
 
-import { fn } from "./fn";
-
-global.fn = fn;
+import parseDate, { type OptionProps } from "./parseDate";
+/**
+ * cronのテキストから、Date型を生成する
+ * @param {string} cronText - cron型のテキスト
+ * @param {object} options - options
+ * @returns {Date[]} cronで生成されたDateリスト
+ */
+global.parseDate = (cronText: string, options?: OptionProps) => parseDate(cronText, options);
